a85f9d53409e15f188bb2a6526d94ba2708fcf72
Javier Pardo <javipardofrias@gmail.com>
2024-06-13 15:00:39 +0200

    Primer commit

---
+++ JavierPardoFrias/JavierPardoFrias/src/main/java/es/ufv/dis/extraordinaria2023/JavierPardoFrias/JavierPardoFriasApplication.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.extraordinaria2023.JavierPardoFrias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavierPardoFriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavierPardoFriasApplication.class, args);
+	}
+
+}
---
+++ JavierPardoFrias/JavierPardoFrias/src/test/java/es/ufv/dis/extraordinaria2023/JavierPardoFrias/JavierPardoFriasApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.extraordinaria2023.JavierPardoFrias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavierPardoFriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
